904a0569a2323f961735eccc786c4a9467a18387
Fábio Almeida <fabio_almeida100@hotmail.com>
2020-09-09 01:06:00 -0300

    Add basic struct of component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import css from './app.module.css';
+import Header from './components/header/Header';
+import Controls from './components/controls/Controls';
+import Transaction from './components/transactions/Transaction';
+import PaginatorMonth from './components/paginator-month/PaginatorMonth';
 
 export default function App() {
-  return <h1>Desafio Final do Bootcamp Full Stack</h1>;
+  return <div className="container">
+    <div className={`center ${css.mt10} ${css.fontTitle} `}>
+        Controle Financeiro
+    </div>
+    <div className={`row center ${css.mt10}`}>
+      <PaginatorMonth />
+    </div>
+    <div className={`row ${css.mt10}`}>
+        <Header />
+    </div>
+    <div className={`row ${css.mt10}`}>
+        <Controls />
+    </div>
+    <div className={`row ${css.mt10}`}>
+        <Transaction />
+    </div>
+  </div>;
 }
