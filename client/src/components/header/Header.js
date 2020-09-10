92bc099112c0e89cb70cdf81364ced46bfad0137
Fábio Almeida <fabio_almeida100@hotmail.com>
2020-09-10 01:19:07 -0300

    Implementation format number in header component

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import css from './header.module.css'
+import { formatNumber } from '../../helper/formatHelpers'
 
 export default function Header({ amoutLancamentoValue, revenueValue, expenseValue, balanceValue }) {
   return (
     <div className={`row ${css.header}`}>
-      <div className="col m3"><div className={css.displayInline}>Lançamentos: </div>{ amoutLancamentoValue }</div>
-      <div className="col m3"><div className={css.displayInline}>Receitas: </div>{ revenueValue }</div>
-      <div className="col m3"><div className={css.displayInline}>Despesas: </div>{ expenseValue }</div>
-      <div className="col m3"><div className={css.displayInline}>Saldo: </div>{ balanceValue }</div>
+      <div className="col m3"><div className={css.displayInline}>Lançamentos: </div>{ formatNumber(amoutLancamentoValue) }</div>
+      <div className="col m3"><div className={css.displayInline}>Receitas: </div>{ formatNumber(revenueValue) }</div>
+      <div className="col m3"><div className={css.displayInline}>Despesas: </div>{ formatNumber(expenseValue) }</div>
+      <div className="col m3"><div className={css.displayInline}>Saldo: </div>{ formatNumber(balanceValue) }</div>
     </div>
   )
 }
