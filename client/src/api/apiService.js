1c7e80d03f711a39d5b9cd8be87aa147f1cd8a0a
Fábio Almeida <fabio_almeida100@hotmail.com>
2020-09-11 22:17:12 -0300

    Add function for call api to insert and update a transaction

--- client/src/api/apiService.js
+++ client/src/api/apiService.js
@@ -7,8 +7,18 @@ const findAll = async (yearMonth) => {
   return await axios.get(`${API_URL}?period=${yearMonth}`);
 }
 
+const insertTransaction = async (transaction) => {
+  const response = await axios.post(API_URL, transaction);
+  return response.data;
+}
+
+const updateTransaction = async (transaction) => {
+  const response = await axios.put(API_URL, transaction);
+  return response.data;
+}
+
 const deleteOne = async (id) => {
   return await axios.delete(`${API_URL}/${id}`);
 }
 
-export { findAll, deleteOne }
+export { findAll, insertTransaction, updateTransaction, deleteOne }
