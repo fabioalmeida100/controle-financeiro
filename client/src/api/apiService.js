72a0b85c56040686ffd74573620376d30f1b41ee
Fábio Almeida <fabio_almeida100@hotmail.com>
2020-09-12 03:42:39 -0300

    Remove console.log from apiService

--- client/src/api/apiService.js
+++ client/src/api/apiService.js
@@ -1,5 +1,4 @@
 import axios from 'axios'
-import dotenv from 'dotenv'
 
 const API_URL = process.env.REACT_APP_API_URL;
 
@@ -11,8 +10,7 @@ const findOne = async (id) => {
   return await axios.get(`${API_URL}/${id}`)
 }
 
-const insertTransaction = async (transaction) => {
-  console.log(transaction);
+const insertTransaction = async (transaction) => {  
   const response = await axios.post(API_URL, transaction);
   return response.data;
 }
