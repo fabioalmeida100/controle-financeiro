c5ddb228f4164125390bbaa2f36e1b02ae535e4d
Fábio Almeida <fabio_almeida100@hotmail.com>
2020-09-15 23:59:45 -0300

    Fix position of button New Transaction

--- client/src/components/button/Button.js
+++ client/src/components/button/Button.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import css from './button.module.css'
 
-export default function Button({handleOpenModal, type}) {
+export default function Button({handleOpenModal}) {
   const internalHandleOpenModal = () => {
     handleOpenModal()
   }
 
   return (
     <div className={css.buttonStyle}>
-      <button style={{width:'100%'}} onClick={internalHandleOpenModal} className="waves-light btn modal-trigger" data-target="modalTransaction">Nova transação</button>
+      <button style={{width:'100%', marginLeft: '-10px'}} onClick={internalHandleOpenModal} className="waves-light btn modal-trigger" data-target="modalTransaction">Nova transação</button>
     </div>
   )
 }
